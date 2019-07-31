e74ffa3183466d6536536114064d024c3ad471a4
Hitarth Sharma <hitarth.sharma@outlook.com>
2019-08-01 09:37:01 +1000

    Added basic styling using styled components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
+import styled from "styled-components/native";
 
 const plays = [
   {
@@ -18,38 +19,52 @@ const plays = [
     id: 3,
     title: "Energy Audit",
     description:
-      "Get the clietn to draw a line in the middle of the page and write the Energy Providers on one side and Energy Drainers on the other. Work on making the Providers list longer than Drainers. Then work on coming up with an approach to counter each and every one of the Drainers and turn it into a Provider."
+      "Get the client to draw a line in the middle of the page and write the Energy Providers on one side and Energy Drainers on the other. Work on making the Providers list longer than Drainers. Then work on coming up with an approach to counter each and every one of the Drainers and turn it into a Provider."
   }
 ];
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+  padding: 50px 15px 10px 15px;
+`;
+
+const H1 = styled.Text`
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+const PlayCard = styled.View`
+  background-color: #eee;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const H3 = styled.Text`
+  font-weight: bold;
+  padding: 5px 0;
+`;
+
+const CopyText = styled.Text``;
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{ fontSize: 32 }}>Coaching Plays</Text>
+      <Container>
+        <H1>Coaching Plays</H1>
         <FlatList
           data={plays}
           keyExtractor={play => play.id.toString()}
           renderItem={({ item: play }) => (
-            <View>
-              <Text style={{ fontWeight: "bold" }}>{play.title}</Text>
-              <Text>{play.description}</Text>
-            </View>
+            <PlayCard>
+              <H3>{play.title}</H3>
+              <CopyText>{play.description}</CopyText>
+            </PlayCard>
           )}
         />
-      </View>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 50,
-    paddingLeft: 15,
-    paddingRight: 15
-  }
-});
